33b37f98c426d7489c13c1a3e2d68e962ebce858
C.J. Adeszko <cjadeszko@gmail.com>
2025-04-09 11:02:01 -0700

    Remove puts

--- lib/receipt.rb
+++ lib/receipt.rb
@@ -6,9 +6,6 @@ class Receipt
   end
 
   def add_item(item)
-    puts "CURRENT ITEMS: #{@items.inspect}"
-    puts "Adding item: #{item.inspect}"
     @items << item
-    puts "ITEMS AFTER ADDING: #{@items.inspect}"
   end
 end
